1ddb2a1223692d728740511117367812618472fb
Jake VanderPlas <jakevdp@google.com>
2019-12-16 06:27:05 -0800

    Add types & refactor driver registry

--- altair_save/compile.py
+++ altair_save/compile.py
@@ -6,30 +6,14 @@ import atexit
 import contextlib
 import os
 import tempfile
+from typing import Any, Dict, Union
+
 import selenium.webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 
 VEGA_VERSION = "5.9.0"
 VEGALITE_VERSION = "4.0.0"
 VEGAEMBED_VERSION = "6.2.1"
-
-
-@contextlib.contextmanager
-def temporary_filename(**kwargs):
-    """Create and clean-up a temporary file
-
-    Arguments are the same as those passed to tempfile.mkstemp
-
-    We could use tempfile.NamedTemporaryFile here, but that causes issues on
-    windows (see https://bugs.python.org/issue14243).
-    """
-    filedescriptor, filename = tempfile.mkstemp(**kwargs)
-    os.close(filedescriptor)
-
-    try:
-        yield filename
-    finally:
-        if os.path.exists(filename):
-            os.remove(filename)
 
 
 HTML_TEMPLATE = """
@@ -90,61 +74,108 @@ if (format === 'vega') {
 """
 
 
-def _get_driver(webdriver, driver_timeout):
-    """Get a webdriver by name."""
-    webdriver = _get_driver.drivers.get(webdriver, webdriver)
-    if isinstance(webdriver, selenium.webdriver.Remote):
-        return webdriver
+@contextlib.contextmanager
+def temporary_filename(**kwargs):
+    """Create and clean-up a temporary file
 
-    if webdriver == "chrome":
-        webdriver_class = selenium.webdriver.Chrome
-        webdriver_options_class = selenium.webdriver.chrome.options.Options
-    elif webdriver == "firefox":
-        webdriver_class = selenium.webdriver.Firefox
-        webdriver_options_class = selenium.webdriver.firefox.options.Options
-    else:
-        raise ValueError(
-            f"Unrecognized webdriver: '{webdriver}'. Expected 'chrome' or 'firefox'"
-        )
+    Arguments are the same as those passed to tempfile.mkstemp
 
-    webdriver_options = webdriver_options_class()
-    webdriver_options.add_argument("--headless")
+    We could use tempfile.NamedTemporaryFile here, but that causes issues on
+    windows (see https://bugs.python.org/issue14243).
+    """
+    filedescriptor, filename = tempfile.mkstemp(**kwargs)
+    os.close(filedescriptor)
 
-    if issubclass(webdriver_class, selenium.webdriver.Chrome):
-        # for linux/osx root user, need to add --no-sandbox option.
-        # since geteuid doesn't exist on windows, we don't check it
-        if hasattr(os, "geteuid") and (os.geteuid() == 0):
-            webdriver_options.add_argument("--no-sandbox")
-
-    driver_obj = webdriver_class(options=webdriver_options)
-    atexit.register(driver_obj.quit)
-    driver_obj.set_page_load_timeout(driver_timeout)
-    _get_driver.drivers[webdriver] = driver_obj
-
-    return driver_obj
+    try:
+        yield filename
+    finally:
+        if os.path.exists(filename):
+            os.remove(filename)
 
 
-_get_driver.drivers = {}
+class _DriverRegistry:
+    """Registry of web driver singletons.
+
+    This prevents the need to start and stop drivers repeatedly.
+    """
+    drivers : Dict[str, WebDriver]
+
+    def __init__(self):
+        self.drivers = {}
+
+    def get(self, webdriver: Union[str, WebDriver], driver_timeout: float) -> WebDriver:
+        """Get a webdriver by name.
+        
+        Parameters
+        ----------
+        webdriver : string or WebDriver
+            The webdriver to use.
+        driver_timeout : float
+            The per-page driver timeout.
+
+        Returns
+        -------
+        webdriver : WebDriver
+        """
+        webdriver = self.drivers.get(webdriver, webdriver)
+        if isinstance(webdriver, WebDriver):
+            return webdriver
+
+        if webdriver == "chrome":
+            webdriver_class = selenium.webdriver.Chrome
+            webdriver_options_class = selenium.webdriver.chrome.options.Options
+        elif webdriver == "firefox":
+            webdriver_class = selenium.webdriver.Firefox
+            webdriver_options_class = selenium.webdriver.firefox.options.Options
+        else:
+            raise ValueError(
+                f"Unrecognized webdriver: '{webdriver}'. Expected 'chrome' or 'firefox'"
+            )
+
+        webdriver_options = webdriver_options_class()
+        webdriver_options.add_argument("--headless")
+
+        if issubclass(webdriver_class, selenium.webdriver.Chrome):
+            # for linux/osx root user, need to add --no-sandbox option.
+            # since geteuid doesn't exist on windows, we don't check it
+            if hasattr(os, "geteuid") and (os.geteuid() == 0):
+                webdriver_options.add_argument("--no-sandbox")
+
+        driver_obj = webdriver_class(options=webdriver_options)
+        atexit.register(driver_obj.quit)
+        driver_obj.set_page_load_timeout(driver_timeout)
+        self.drivers[webdriver] = driver_obj
+
+        return driver_obj
+
+
+_registry = _DriverRegistry()
 
 
 def compile_spec(
-    spec,
-    fmt,
-    mode,
-    vega_version=VEGA_VERSION,
-    vegaembed_version=VEGAEMBED_VERSION,
-    vegalite_version=VEGALITE_VERSION,
-    scale_factor=1,
-    driver_timeout=20,
-    webdriver="chrome",
-):
+    spec: Dict[str, Any],
+    fmt: str,
+    mode: str,
+    vega_version: str = VEGA_VERSION,
+    vegaembed_version: str = VEGAEMBED_VERSION,
+    vegalite_version: str = VEGALITE_VERSION,
+    scale_factor: float = 1,
+    driver_timeout: float = 20,
+    webdriver: str = "chrome",
+) -> str:
+    """Use selenium to compile a vega or vega-lite spec.
+
+    Parameters
+    ----------
+
+    """
     if fmt not in ["png", "svg", "vega"]:
         raise NotImplementedError(f"fmt={fmt!r} must be 'svg', 'png' or 'vega'")
 
     if mode not in ["vega", "vega-lite"]:
         raise ValueError("mode must be either 'vega' or 'vega-lite'")
 
-    driver = _get_driver(webdriver, driver_timeout)
+    driver = _registry.get(webdriver, driver_timeout)
 
     html = HTML_TEMPLATE.format(
         vega_version=vega_version,
--- altair_save/core.py
+++ altair_save/core.py
@@ -1,11 +1,13 @@
 import base64
 import json
+from typing import Dict, IO, Union
 
 import altair as alt
 from altair_save.compile import compile_spec
 
 
-def _write_file_or_filename(fp, content, mode="w"):
+# TODO: make this only accept binary
+def _write_file_or_filename(fp: Union[IO, str], content: Union[str, bytes], mode="w"):
     """Write content to fp, whether fp is a string or a file-like object"""
     if isinstance(fp, str):
         with open(fp, mode) as f:
@@ -19,22 +21,23 @@ class Saver:
         self.chart = chart
         self._kwargs = kwargs
 
-    def mimebundle(self, fmt):
+    def mimebundle(self, fmt: str) -> Dict[str, Union[str, bytes]]:
         if isinstance(self.chart, dict):
             spec = self.chart
         else:
             spec = self.chart.to_dict()
         out = compile_spec(spec, fmt, mode="vega-lite")
+        print(fmt, type(out))
         if fmt == "png":
             return {"image/png": base64.b64decode(out.split(",", 1)[1].encode())}
         elif fmt == "svg":
-            return {"image/svg+xml": out}
+            return {"image/svg+xml": out.encode()}
         elif fmt == "vega":
             return {"application/vnd.vega.v{}+json".format(alt.VEGA_VERSION[0]): out}
         else:
             raise ValueError(f"Unrecognized format: {fmt}")
 
-    def save(self, fp, fmt=None):
+    def save(self, fp: Union[IO, str], fmt: str = None):
         if fmt is None:
             if isinstance(fp, str):
                 fmt = fp.split(".")[-1]
@@ -44,7 +47,7 @@ class Saver:
         if fmt == "png":
             _write_file_or_filename(fp, mimebundle["image/png"], mode="wb")
         elif fmt == "svg":
-            _write_file_or_filename(fp, mimebundle["image/svg+xml"], mode="w")
+            _write_file_or_filename(fp, mimebundle["image/svg+xml"], mode="wb")
         elif fmt == "vega":
             _write_file_or_filename(
                 fp, json.dumps(mimebundle.popitem()[1], indent=2), mode="w"
