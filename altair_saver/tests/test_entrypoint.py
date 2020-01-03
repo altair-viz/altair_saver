e95a25d8a57518e37a87a2c7edab3a5403c0f922
Jake VanderPlas <jakevdp@google.com>
2020-01-03 09:07:17 -0800

    ENH: add altair_saver entrypoint

---
+++ altair_saver/tests/test_entrypoint.py
@@ -0,0 +1,16 @@
+import altair as alt
+from altair import vega
+from altair_saver import render
+
+
+def test_entrypoint_exists():
+    assert "altair_saver" in alt.renderers.names()
+    assert "altair_saver" in vega.renderers.names()
+
+
+def test_entrypoint_identity():
+    with alt.renderers.enable("altair_saver"):
+        assert alt.renderers.get() is render
+
+    with vega.renderers.enable("altair_saver"):
+        assert vega.renderers.get() is render
